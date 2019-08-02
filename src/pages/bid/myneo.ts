cd8255b118547a6b01187ad133801cca69f5fd3d
dabanli123 <1031737848@qq.com>
2019-08-02 16:24:21 +0800

    修复我的域名管理页自动返回第一页问题

--- src/pages/bid/myneo.ts
+++ src/pages/bid/myneo.ts
@@ -117,13 +117,15 @@ export default class MyNeo extends Vue
         this.getMyNNC();
         // this.getAllNeoName();
         this.getBindDomain();
-        this.getMyDomainList(true);
+        // this.getMyDomainList(true);
         if (this.isFirstFlag)
         {
+            this.getMyDomainList(true);
             this.getSaleDomainList(this.currentAddress, true, this.salePage);
             this.isFirstFlag = false;
         } else
         {
+            this.getMyDomainList(false);
             this.getSaleDomainList(this.currentAddress, false, this.salePage);
         }
     }
